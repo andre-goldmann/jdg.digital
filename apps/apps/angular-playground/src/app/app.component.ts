f1874eb71cd5d9bc6722591c174163a4dd54baee
agol <andre.goldmann@hacon.de>
2025-03-28 11:38:20 +0100

    initial nx

---
+++ apps/apps/angular-playground/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  imports: [RouterModule, MatButton],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'angular-playground';
+}
