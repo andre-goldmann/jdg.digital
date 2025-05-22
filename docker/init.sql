3099ded104872f9a03159261f5f69a94c2ee40a9
andre-goldmann <andre_goldmann@yahoo.de>
2025-05-22 09:11:03 +0200

    n8n docker compose

--- docker/init.sql
+++ docker/init.sql
@@ -2,7 +2,8 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE IF NOT EXISTS embeddings (
                                       id SERIAL PRIMARY KEY,
-                                      embedding vector,
+                                      embedding vector(1536),
                                       text text,
+                                      metadata jsonb,
                                       created_at timestamptz DEFAULT now()
 );
