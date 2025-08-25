6ab1f1ba6525b9303e68380a9d42f71c3bbdd7a0
agol <andre.goldmann@hacon.de>
2025-08-25 15:43:52 +0200

    feat(server): add server entry point and configure Vercel settings for API routing

---
+++ apps/api/index.js
@@ -0,0 +1,12 @@
+//export default import('../dist/apps/wellsync/server/server.mjs').then(module => module.app);
+
+//const server = require('../dist/apps/wellsync/server/main');
+
+//module.exports = server.app();
+
+// Import required modules
+const path = require('path');
+
+const serverDistPath = path.join(process.cwd(), 'dist/wellsync/server/server.mjs');
+
+export default import(serverDistPath).then(module => module.app);
