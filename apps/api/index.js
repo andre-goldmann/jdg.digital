4a558d02a0d01e0350bb9646575cea559d9834cc
agol <andre.goldmann@hacon.de>
2025-08-25 16:08:28 +0200

    fix(server): update server import path and clean up commented code

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,12 +1,12 @@
 //export default import('../dist/apps/wellsync/server/server.mjs').then(module => module.app);
 
-//const server = require('../dist/apps/wellsync/server/main');
+const server = require('../dist/apps/wellsync/server/server.mjs');
 
-//module.exports = server.app();
+module.exports = server.app();
 
 // Import required modules
-const path = require('path');
+//const path = require('path');
 
-const serverDistPath = path.join(process.cwd(), 'dist/wellsync/server/server.mjs');
+//const serverDistPath = path.join(process.cwd(), 'dist/wellsync/server/server.mjs');
 
-export default import(serverDistPath).then(module => module.app);
+//export default import(serverDistPath).then(module => module.app);
