f3bfe2c3267f2b373a5a1529f8ae78142a62af68
agol <andre.goldmann@hacon.de>
2025-08-25 15:21:07 +0200

    fix(server): update server import and adjust Vercel configuration for correct file inclusion

--- apps/apps/wellsync/api/index.js
+++ apps/apps/wellsync/api/index.js
@@ -1,1 +1,5 @@
-export default import('../dist/apps/wellsync/server/server.mjs').then(module => module.app);
+//export default import('../dist/apps/wellsync/server/server.mjs').then(module => module.app);
+
+const server = require('../dist/apps/wellsync/server/main');
+
+module.exports = server.app();
