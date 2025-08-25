8e7ef4fdd3d19bbe058153bc1fecd95627c017bc
agol <andre.goldmann@hacon.de>
2025-08-25 16:32:50 +0200

    fix(index): remove commented code and simplify server import for clarity

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,12 +1,1 @@
 export default import('../dist/apps/wellsync/server/server.mjs').then(module => module.app);
-
-//const server = require('../dist/apps/wellsync/server/server.mjs');
-
-//module.exports = server.app();
-
-// Import required modules
-//const path = require('path');
-
-//const serverDistPath = path.join(process.cwd(), 'dist/wellsync/server/server.mjs');
-
-//export default import(serverDistPath).then(module => module.app);
